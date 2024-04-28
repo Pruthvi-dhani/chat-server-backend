b21dbfc5f06486716e7a79c45dce4b43649a44a3
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2024-04-29 00:05:49 +0100

    create mysql table schema

---
+++ sql_statements/version_1.sql
@@ -0,0 +1,60 @@
+-- create users table
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `display_name` varchar(256) NOT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `is_deleted` tinyint NOT NULL DEFAULT '0',
+  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+);
+
+-- create groups table
+CREATE TABLE `chat_app`.`group_chats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `group_name` VARCHAR(100) NOT NULL,
+  `group_type` ENUM('private_conv', 'group_conv') NOT NULL DEFAULT 'private_conv',
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+  `created_time` DATETIME NOT NULL DEFAULT  CURRENT_TIMESTAMP,
+  `updated_time` DATETIME NULL DEFAULT  CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`));
+
+-- create conversations table
+CREATE TABLE `chat_app`.`conversations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `group_id` INT NOT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+  `created_time` DATETIME NOT NULL DEFAULT current_timestamp,
+  `updated_time` DATETIME NULL DEFAULT current_timestamp on update current_timestamp,
+  PRIMARY KEY (`id`),
+  INDEX `group_id_fk_idx_idx` (`group_id` ASC) VISIBLE,
+  INDEX `user_id_fk_idx_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `group_id_fk_idx`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `chat_app`.`group_chats` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `user_id_fk_idx`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `chat_app`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+-- create message table
+CREATE TABLE `chat_app`.`messages` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `group_id` INT NOT NULL,
+  `message_content` VARCHAR(2048) NOT NULL,
+  `is_deleted` TINYINT NOT NULL DEFAULT 0,
+  `created_time` DATETIME NOT NULL DEFAULT current_timestamp,
+  `updated_time` DATETIME NULL DEFAULT current_timestamp on update current_timestamp,
+  PRIMARY KEY (`id`),
+  INDEX `conv_id_fk_idx_idx` (`group_id` ASC) VISIBLE,
+  CONSTRAINT `conv_id_fk_idx`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `chat_app`.`conversations` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
